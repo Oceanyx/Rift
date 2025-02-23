610c6df7442f0f54769624e84a69fab0282f6d70
OceanicPhoenix <runninggrandmaster@gmail.com>
2025-02-22 16:14:25 -0800

    validate done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,10 +4,52 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
-// Log in
-router.post(
+//Logout
+router.delete(
     '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+        return res.json({
+            user: safeUser
+        });
+        } else return res.json({ user: null });
+    }
+);
+
+//Login validation middleware
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+//Log In with Validation
+  router.post(
+    '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -42,30 +84,4 @@ router.post(
     }
   );
 
-//Logout
-router.delete(
-    '/',
-    (_req, res) => {
-        res.clearCookie('token');
-        return res.json({ message: 'success' });
-    }
-);
-
-// Restore session user
-router.get(
-    '/',
-    (req, res) => {
-        const { user } = req;
-        if (user) {
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-        return res.json({
-            user: safeUser
-        });
-        } else return res.json({ user: null });
-    }
-);
   module.exports = router;
